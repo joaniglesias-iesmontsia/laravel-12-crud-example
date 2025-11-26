6b81e7a56497bce6fd34dae025e4e7289661ec6c
joaniglesias-iesmontsia <joaniglesias@iesmontsia.org>
2025-11-26 20:29:32 +0100

    Add PostFactory and PostSeeder for seeding posts in the database

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,18 @@
+<?php
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
+
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraphs(3, true),
+        ];
+    }
+}
